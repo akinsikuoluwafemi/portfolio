b07c2cc8f66defbccb730ea1e90f8ecdf4f69025
Akinsiku Femi <akinsiku13@gmail.com>
2019-10-21 12:07:34 +0100

    my first commit

---
+++ app/app.js
@@ -0,0 +1,65 @@
+// select elements;
+let hamburger = document.querySelector('span');
+let cancel = document.querySelector('.cancel');
+let wrapper = document.querySelector('.wrapper');
+let wrap = document.querySelector('.wrap');
+
+// let appear = true;
+
+// hamburger.addEventListener('click', function(e) {
+//     if(appear){
+//         wrapper.style.opacity = '0';
+//         wrapper.style.transition = 'all 1s';
+
+//     setTimeout(() => {
+//     wrap.style.visibility = 'visible';
+    
+//     }, 700);
+//         appear = false;
+
+        
+// }
+// else {
+//         wrapper.style.opacity = '1'; 
+//         wrapper.style.transition = 'all 1s';
+        
+//          setTimeout(() => {
+//         wrap.style.visibility = 'hidden';
+    
+//     }, 700);
+//  }
+
+// })
+
+
+// cancel.addEventListener('click', function() {
+//     if(appear){
+//         wrap.style.opacity = '0';
+//         wrap.style.transition = 'all 1s';
+
+//     setTimeout(() => {
+//     wrapper.style.visibility = 'visible';
+    
+//     }, 700);
+//         wrapper.style.opacity = '1';
+//         appear = false;
+        
+//     }
+//     else {
+//         wrapper.style.opacity = 0; 
+//         wrapper.style.transition = 'all 1s';
+//         wrapper.style.visibility = 'hidden';
+
+
+//         wrap.style.visibility = 'hidden';
+//         wrap.style.opacity = '1';
+//         wrap.style.transition = 'all 1s ease';
+         
+
+
+//     }
+    
+
+
+// })
+
