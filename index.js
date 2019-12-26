c0b54392a17fe5d8e7389616e39e5b4ed8972fd9
Akinsiku Femi <akinsiku13@gmail.com>
2019-12-26 13:11:44 +0100

    add media query to portfolio and writings page

--- index.js
+++ index.js
@@ -7,10 +7,28 @@ const eye = document.querySelector('.eye');
 
 
 const hide = document.querySelector('.hide');
+const hides = document.querySelector('.hides');
+
 const hideOne = document.querySelector('.hide-one');
+const hidesOne = document.querySelector('.hides-one');
+
 const hideTwo = document.querySelector('.hide-two');
 const hideThree = document.querySelector('.hide-three');
 const hideFour = document.querySelector('.hide-four');
+const displayImage = document.querySelectorAll('.display-image');
+
+// if (!("ontouchstart" in document.documentElement)) {
+// document.documentElement.className += " no-touch";
+// }
+
+// mouseleave event when you hover off the images on the portfolio
+displayImage.forEach(el => {
+    el.addEventListener('mouseleave', (e) => {
+        e.target.style.transition = "all 1.5s ease-out";
+    })
+})
+
+
 
 
 //loop through all the icons and apply the event listener to it when clicked
@@ -33,13 +51,27 @@ got.addEventListener('click', function (e){
         hideTwo.style.display = "none"; 
         hideThree.style.display = "none"; 
 
-
-
-
-
         
     } else {
         hide.style.display = "none"; 
+    }
+});
+
+// for writings
+got.addEventListener('click', function (e){
+    if (hides.style.display === "none") {
+        hides.style.display = "block";
+        hides.style.display = "grid";
+        hides.style.animation = "movetoright 2.2s";
+        hideOne.style.display = "none"; 
+        hidesOne.style.display = "none"; 
+
+        hideTwo.style.display = "none"; 
+        hideThree.style.display = "none"; 
+
+        
+    } else {
+        hides.style.display = "none"; 
     }
 });
 
@@ -49,7 +81,9 @@ med.addEventListener('click', function(e) {
         hideOne.style.display = "block";
         hideOne.style.display = "grid";
         hideOne.style.animation = "movetoleft 2.2s";
-         hide.style.display = "none";  
+         hide.style.display = "none"; 
+         hides.style.display = "none";  
+         
          hideTwo.style.display = "none"; 
          hideThree.style.display = "none";  
          hideFour.style.display = "none";  
@@ -63,13 +97,39 @@ med.addEventListener('click', function(e) {
 })
 
 
+med.addEventListener('click', function(e) {
+     if (hidesOne.style.display === "none") {
+        hidesOne.style.display = "block";
+        hidesOne.style.display = "grid";
+        hidesOne.style.animation = "movetoleft 2.2s";
+         hide.style.display = "none"; 
+         hides.style.display = "none";  
+         
+         hideTwo.style.display = "none"; 
+         hideThree.style.display = "none";  
+         hideFour.style.display = "none";  
+         
+         
+
+        
+    } else {
+        hidesOne.style.display = "none"; 
+    }
+})
+
+
+
 every.addEventListener('click', function(e) {
       if (hideTwo.style.display === "none") {
         hideTwo.style.display = "block";
         hideTwo.style.display = "grid";
         hideTwo.style.animation = "movetoright 2.2s";
-        hide.style.display = "none";  
-        hideOne.style.display = "none";  
+          hide.style.display = "none";
+          hides.style.display = "none";  
+          
+          hideOne.style.display = "none";
+          hidesOne.style.display = "none";  
+          
         hideFour.style.display = "none"; 
         hideThree.style.display = "none"; 
           
@@ -88,8 +148,12 @@ kry.addEventListener('click', (e) =>{
         hideThree.style.display = "block";
         hideThree.style.display = "grid";
         hideThree.style.animation = "movetoleft 2.2s";
-        hide.style.display = "none";  
-        hideOne.style.display = "none";  
+          hide.style.display = "none";
+          hides.style.display = "none";  
+          
+          hideOne.style.display = "none"; 
+          hidesOne.style.display = "none";  
+          
         hideTwo.style.display = "none";  
         hideFour.style.display = "none";  
           
@@ -107,8 +171,12 @@ eye.addEventListener('click', (e)=> {
         hideFour.style.display = "block";
         hideFour.style.display = "grid";
         hideFour.style.animation = "movetoright 2.2s";
-        hide.style.display = "none";  
-        hideOne.style.display = "none";  
+           hide.style.display = "none";
+           hides.style.display = "none";  
+           
+           hideOne.style.display = "none";
+           hidesOne.style.display = "none";  
+           
         hideTwo.style.display = "none";  
         hideThree.style.display = "none";  
           
